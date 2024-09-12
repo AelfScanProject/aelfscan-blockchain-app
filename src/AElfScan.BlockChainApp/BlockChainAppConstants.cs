0050023c96512471cb5a1f5b5a92e26263d9b681
alanexplore <alanexplore@aelfscan.io>
2024-09-12 13:37:51 +0800

    Update transaction list query

--- src/AElfScan.BlockChainApp/BlockChainAppConstants.cs
+++ src/AElfScan.BlockChainApp/BlockChainAppConstants.cs
@@ -5,16 +5,14 @@ public class BlockChainAppConstants
     public static Dictionary<string, string> ContractAddresses = new()
     {
         { "AELF", "JRmBduh4nXWi1aXgdUsj5gJrzeZb2LxmrAbf7W99faZSvoAaE" },
-        { "tDVW", "ASh2Wt7nSEmYqnGxPPzp4pnVDU4uhj1XW9Se5VeZcX2UDdyjx" }
+        { "tDVV", "7RzVGiuVWkvL4VfVHdZfQF2Tri3sgLe9U991bohHFfSRZXuGX" }
     };
-
-
-    public static Dictionary<string, long> TransactionBeginHeight = new Dictionary<string, long>
+    public static Dictionary<string,long> TransactionBeginHeight = new Dictionary<string, long>
     {
-        { "AELF", 193837741 },
-        { "tDVW", 133412351 }
+        { "AELF", 217634830 },
+        { "tDVV", 205591099 }
     };
-
+    
     public static readonly Dictionary<string, List<string>> TransactionAddressListMap = new()
     {
         {
@@ -22,18 +20,17 @@ public class BlockChainAppConstants
             {
                 "JRmBduh4nXWi1aXgdUsj5gJrzeZb2LxmrAbf7W99faZSvoAaE",
                 "pGa4e5hNGsgkfjEGm72TEvbF7aRDqKBd4LuXtab4ucMbXLcgJ",
-                "2JT8xzjR5zJ8xnBvdgBZdSjfbokFSbF5hDdpUCbXeWaJfPDmsK",
-                "2SQ9LeGZYSWmfJcYuQkDQxgd3HzwjamAaaL4Tge2eFSXw2cseq"
+                "2JT8xzjR5zJ8xnBvdgBZdSjfbokFSbF5hDdpUCbXeWaJfPDmsK"
             }
         },
         {
-            "tDVW", new List<string>()
+            "tDVV", new List<string>()
             {
-                "2KPUA5wG78nnNmK9JsRWbFiEFUEfei9WKniZuKaVziDKZRwchM",
-                "ASh2Wt7nSEmYqnGxPPzp4pnVDU4uhj1XW9Se5VeZcX2UDdyjx",
-                "2Z2cfgvH4QkL8KsEKegYJRJ4JC3QYkCVVptF1mvtQVNgZXRL6u",
-                "2PC7Jhb5V6iZXxz8uQUWvWubYkAoCVhtRGSL7VhTWX85R8DBuN"
+                "7RzVGiuVWkvL4VfVHdZfQF2Tri3sgLe9U991bohHFfSRZXuGX",
+                "BNPFPPwQ3DE9rwxzdY61Q2utU9FZx9KYUnrYHQqCR6N4LLhUE",
+                "4SGo3CUj3PPh3hC5oXV83WodyUaPHuz4trLoSTGFnxe84nqNr"
             }
         }
     };
+    
 }
